09f05c780077dba60859dc1c26e7e8943ce124dd
Tony Hirst <tony.hirst@gmail.com>
2025-07-23 13:24:06 +0100

    Create builder.py

---
+++ jupyterlite_builder/builder.py
@@ -0,0 +1,13 @@
+# Build a JupyterLite distribution from an oucb template
+
+# Apt packages
+# - can these be replaced by wasm packages?
+# - if not, thiese cannot be installed.
+
+# Py packages:
+# Are these available:
+# - in pyodide?
+# - as `none-any` wheels?
+# - in Emscripten-forge? https://beta.mamba.pm/channels/emscripten-forge
+
+# If packages are in pyodide or `none-any` wheels, we should be able to use pyodide kernel. (Maybe need an option to set kernel)
